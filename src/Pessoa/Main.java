81241bb6612244088c3c22bd34b5edd3bee34bd9
Adrian-Goulart <adrianx2007@hotmail.com>
2025-11-11 19:27:07 -0300

    classe Pessoa 2

--- src/Pessoa/Main.java
+++ src/Pessoa/Main.java
@@ -5,5 +5,23 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+
+        Pessoa pessoaUm = new Pessoa();
+        pessoaUm.setNome("Adrian");
+        pessoaUm.setIdade(18);
+
+        Pessoa pessoaDois = new Pessoa();
+        pessoaDois.setNome("Kauã");
+        pessoaDois.setIdade(18);
+
+        Pessoa pessoaTres = new Pessoa();
+        pessoaTres.setNome("Lucas");
+        pessoaTres.setIdade(18);
+
+        listaPessoas.add(pessoaUm);
+        listaPessoas.add(pessoaDois);
+        listaPessoas.add(pessoaTres);
+
+        System.out.println("Tamanho da lista: " + listaPessoas.size());
     }
 }
--- src/Pessoa/Pessoa.java
+++ src/Pessoa/Pessoa.java
@@ -22,6 +22,6 @@ public class Pessoa {
 
     @Override
     public String toString() {
-        return "Nome: " + nome + "| Idade: " + idade;
+        return "Nome: " + nome + " | Idade: " + idade;
     }
 }
